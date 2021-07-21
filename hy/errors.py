5af6130131abe4e89422d249524592f59b7b03a2
Allison Casey <allie.jo.casey@gmail.com>
2021-07-20 21:53:27 -0400

    Fix: filter internal importlib frames from stacktrace

--- hy/errors.py
+++ hy/errors.py
@@ -8,6 +8,7 @@ from functools import reduce
 from colorama import Fore
 from contextlib import contextmanager
 from hy import _initialize_env_var
+from hy._compat import PYPY
 
 _hy_filter_internal_errors = _initialize_env_var('HY_FILTER_INTERNAL_ERRORS',
                                                  True)
@@ -237,6 +238,20 @@ _tb_hidden_modules = {m for m in map(_module_filter_name,
                                       'hy.core.result_macros',
                                       'rply'])
                       if m is not None}
+
+# We can't derive these easily from just their module names due
+# to missing magic attributes in internal importlib modules
+if PYPY:
+    _tb_hidden_modules.update(
+        [
+            "<builtin>/frozen importlib._bootstrap_external",
+            "<builtin>/frozen importlib._bootstrap",
+        ]
+    )
+else:
+    _tb_hidden_modules.update(
+        ["<frozen importlib._bootstrap_external>", "<frozen importlib._bootstrap>"]
+    )
 
 
 def hy_exc_filter(exc_type, exc_value, exc_traceback):
